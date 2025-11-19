700aedd06889364bc802dfbe172992c0c9206cd8
paulasierraa <paulacode01@gmail.com>
2025-11-18 19:09:17 -0500

    Home whatsapp messages

---
+++ src/utils/whatsapp.ts
@@ -0,0 +1,28 @@
+const phone = "573011090186";
+
+export const whatsappLinks = {
+    cotizarSeguro: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero cotizar un seguro"
+    )}`,
+    cotizarPoliza: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero cotizar una póliza"
+    )}`,
+    cotizarPlanSeguridadSocial: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero afiliarme a la seguridad social"
+    )}`,
+    cotizarEps: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero afiliarme a solo EPS"
+    )}`,
+    cotizarArl: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero afiliarme a solo ARL"
+    )}`,
+    cotizarArlEmpresas: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero una asesoría gratuita para mi empresa"
+    )}`,
+    hablarConAsesor: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero hablar con un asesor"
+    )}`,
+    asesoriaCreditoVehicular: `https://wa.me/${phone}?text=${encodeURIComponent(
+        "Hola, vi el sitio web y quiero una asesoría gratuita para mi crédito vehicular"
+    )}`,
+};
